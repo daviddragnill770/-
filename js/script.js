6d709dc239c676c387a1ab79b5159c273efa1681
david <mosslak21@gmail.com>
2023-04-18 19:30:21 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let number = 5;
+
+console.log(55/0);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "David",
+    age: 20,
+    colleg: 'Turo',
+}
+
+console.log(obj["name"]);
+
+const result = confirm("ты долбоёеб?");
+console.log(result)
+
+const answer = prompt("тебе сколько лет, уёбоек?", "21")
+console.log(answer)
+
+const answers = []
+
+answers[0] = prompt("how your name?")
+answers[1] = prompt("how your surname?")
+answers[2] = prompt("ты долбоёб?")
+
+document.write(answers)
+console.log(typeof(answers))
+
+const category = "toys";
+
+console.log(`https://someurl.com/${category}/5`)
+
+const user = "david"
+
+alert(`hello, ${user}`)
+
+console.log(5 * +"3") 
+
+let incr = 10,
+    decr = 10;
+
+incr--;
+decr++;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2)
