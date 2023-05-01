db28fec521f07e8009f01568f65f4922a7faba00
david <mosslak21@gmail.com>
2023-05-02 00:40:07 +0300

    david

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-const number0fFilms = +prompt("Сколько фильмов ты посмотрел?", "");
+// const number0fFilms = +prompt("Сколько фильмов ты посмотрел?", "");
 
-const personalMovieDB = {
-    count: number0fFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: number0fFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 
-// for (let i = 0; i < 1; i++){
+// // for (let i = 0; i < 1; i++){
+// //     const a = prompt("Один из последних просмотренных  фильмов?", ""),
+// //           b = prompt("На сколько оцените его?", "");
+
+// //     if (a != "" && b != "" && a != null && b != null && a.length < 50){
+// //         personalMovieDB.movies[a] = b;
+// //         console.log("done");
+// //     } else {
+// //         console.log("error");
+// //         i--;
+// //     }     
+// // }
+
+// let num = 0;
+
+// while (num < 2){
+//     console.log(num);
 //     const a = prompt("Один из последних просмотренных  фильмов?", ""),
 //           b = prompt("На сколько оцените его?", "");
 
 //     if (a != "" && b != "" && a != null && b != null && a.length < 50){
 //         personalMovieDB.movies[a] = b;
-//         console.log("done");
+//         console.log("done"); 
+//         num++;
 //     } else {
 //         console.log("error");
-//         i--;
-//     }     
+//         num--;
+//     }   
 // }
 
-let num = 0;
-
-while (num < 2){
-    console.log(num);
-    const a = prompt("Один из последних просмотренных  фильмов?", ""),
-          b = prompt("На сколько оцените его?", "");
-
-    if (a != "" && b != "" && a != null && b != null && a.length < 50){
-        personalMovieDB.movies[a] = b;
-        console.log("done"); 
-        num++;
-    } else {
-        console.log("error");
-        num--;
-    }   
-}
-
-if (personalMovieDB.count < 10){
-    console.log("мало фильм посмотрел, сука");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-        console.log("надо больше фильмов смотреть, петух");
-    } else if (personalMovieDB.count > 30){
-        console.log("средненькое колличество долбоеб");
-    } else {
-        console.log("ошибка нахуй")
-    }
+// if (personalMovieDB.count < 10){
+//     console.log("мало фильм посмотрел, сука");
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+//         console.log("надо больше фильмов смотреть, петух");
+//     } else if (personalMovieDB.count > 30){
+//         console.log("средненькое колличество долбоеб");
+//     } else {
+//         console.log("ошибка нахуй")
+//     }
 
 
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 
 
