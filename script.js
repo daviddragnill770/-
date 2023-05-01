90000af4be0a4c6f235deeb9c336de6a6c39eecf
david <mosslak21@gmail.com>
2023-05-01 10:53:23 +0300

    xyeta

--- script.js
+++ script.js
@@ -8,12 +8,50 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных  фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных  фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// for (let i = 0; i < 1; i++){
+//     const a = prompt("Один из последних просмотренных  фильмов?", ""),
+//           b = prompt("На сколько оцените его?", "");
+
+//     if (a != "" && b != "" && a != null && b != null && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//     } else {
+//         console.log("error");
+//         i--;
+//     }     
+// }
+
+let num = 0;
+
+while (num < 2){
+    console.log(num);
+    const a = prompt("Один из последних просмотренных  фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+
+    if (a != "" && b != "" && a != null && b != null && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log("done"); 
+        num++;
+    } else {
+        console.log("error");
+        num--;
+    }   
+}
+
+if (personalMovieDB.count < 10){
+    console.log("мало фильм посмотрел, сука");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log("надо больше фильмов смотреть, петух");
+    } else if (personalMovieDB.count > 30){
+        console.log("средненькое колличество долбоеб");
+    } else {
+        console.log("ошибка нахуй")
+    }
+
+
 
 console.log(personalMovieDB);
+
+
+
